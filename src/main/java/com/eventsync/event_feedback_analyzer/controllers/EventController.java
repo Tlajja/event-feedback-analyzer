fde19335ff7f61f6f508d8a11aa5ab42a98b25c9
Eigintas Urbanavičius <eigintas.urbanavicius@mif.stud.vu.lt>
2025-11-13 19:35:14 +0200

    Created Event controller with simple mappings

---
+++ src/main/java/com/eventsync/event_feedback_analyzer/controllers/EventController.java
@@ -0,0 +1,48 @@
+package com.eventsync.event_feedback_analyzer.controllers;
+
+import com.eventsync.event_feedback_analyzer.services.EventService;
+import com.eventsync.event_feedback_analyzer.dtos.*;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/events")
+public class EventController {
+    private final EventService eventService;
+
+    public EventController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    // POST /events - Create new event
+    @PostMapping
+    public ResponseEntity<EventResponse> createEvent(@RequestBody CreateEventRequest request) {
+        EventResponse eventResponse = eventService.createEvent(request);
+        return ResponseEntity.ok(eventResponse);
+    }
+
+    // Get /events - Get all events
+    @GetMapping
+    public ResponseEntity<List<EventResponse>> getAllEvents() {
+        List<EventResponse> eventResponses = eventService.getAllEvents();
+        return ResponseEntity.ok(eventResponses);
+    }
+
+    // Get /events/{id} - Get event by id
+    @GetMapping("/{id}")
+    public ResponseEntity<EventResponse> getEventById(@PathVariable long id) {
+        EventResponse eventResponse = eventService.getEventResponseById(id);
+        return eventResponse != null ? ResponseEntity.ok(eventResponse) : ResponseEntity.notFound().build();
+    }
+
+    // Get /events/{id}/summary - Get event summary by id
+    @GetMapping("/{id}/summary")
+    public ResponseEntity<EventSummaryResponse> getEventSummary(@PathVariable long id) {
+        EventSummaryResponse eventSummaryResponse = eventService.getEventSummary(id);
+        return eventSummaryResponse != null ? ResponseEntity.ok(eventSummaryResponse)
+                : ResponseEntity.notFound().build();
+    }
+}
