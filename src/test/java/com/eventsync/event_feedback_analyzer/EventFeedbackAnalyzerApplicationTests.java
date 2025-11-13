b289f84a100e7bfa7288c730150238d59ae25dab
Eigintas Urbanavičius <eigintas.urbanavicius@mif.stud.vu.lt>
2025-11-13 15:06:24 +0200

    Created project with Spring Initializr web-based tool

---
+++ src/test/java/com/eventsync/event_feedback_analyzer/EventFeedbackAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventsync.event_feedback_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventFeedbackAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
